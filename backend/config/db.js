28447cef8f511215cf398d84dc5d87e25091a045
Swastikmaity-2004 <your-email@example.com>
2025-12-07 11:32:46 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in .env");
+    }
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
