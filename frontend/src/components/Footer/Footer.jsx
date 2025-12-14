fbe7cd9dfe0bd17db3d0fa9e8080b0a30638ece5
Swastik Maity <swastikmaity03@gmail.com>
2025-12-14 11:53:06 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -33,13 +33,13 @@ const Footer = () => {
           <h2>Get in touch</h2>
           <ul>
             <li>+92-308-4900522</li>
-            <li>contact@tomato.com</li>
+            <li>contact@food call.com</li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 @ Tomato.com - All Right Reserved.
+        Copyright 2024 @ food call.com - All Right Reserved.
       </p>
     </div>
   );
